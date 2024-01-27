4818e1fa38ba8bfc099d0cf7337b24b72c62dc45
Mack <bob9531@gmail.com>
2024-01-27 10:29:43 +0800

    Complete the whole function that extracting the tar.gz file and split the inside mbox to eml files.

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os
 import re
-import mailbox
 import tarfile
-import pathlib
+
+import mailbox
 from email.utils import parsedate_to_datetime
 
 
-proj_dir = os.path.dirname(__file__)
-list_name = '6lo'
+project_dir = os.path.dirname(__file__)
 
 # 解壓縮資料檔
 # unpack the compressed data file
@@ -23,8 +22,6 @@ def extract_tarfile(file_path, out_dir="."):
     # item_files = [item.name for item in tar]
     tar.extractall(out_dir)
     
-
-
     # close the file
     tar.close()
 
@@ -34,26 +31,32 @@ def mbox_to_eml(mboxfile_path, out_dir='.') -> None:
     # new folder name built by mbox
     folder_name = re.search(r"\d\d\d\d-\d\d", mboxfile_path).group()
 
+    # create folders to store email files by mbox
+    os.makedirs(out_dir, exist_ok=True)
+    os.makedirs(f"{out_dir}/{folder_name}", exist_ok=True)
+
     # read the .mbox file and split it
     for index, message in enumerate(mailbox.mbox(mboxfile_path), 1):
 
-        # create folders to store email files by mbox
-        if not os.path.exists(folder_name):
-            os.makedirs(f"{out_dir}", exist_ok=True)
-        
         # create email files one by one 
-        with open(f"{out_dir}/{folder_name}-{index:0d}.eml", "w", encoding="utf-8") as file:
+        with open(f"{out_dir}/{folder_name}/{folder_name}-{index}.eml", "w", encoding="utf-8") as file:
             file.write(message.as_string())
 
 
+# converting one mbox file to emls files to target dir
 def main():
-    data_dirname = "data"
+    list_name = '6lo'
+    resource_dir = "resource" # store extracted data
+    mailbox_dir = "emails" # store .eml files dsorted by mbox
     tar_file = 'mbox0122_ast_b.tar.gz'
-    mbox_file_path = f"{data_dirname}/{list_name}/{tar_file[:-7]}/2013-05.mbox" # data\6lo\2013-05.mbox
-    mbox_dir = re.search(r"")
+    tar_path = f"{resource_dir}/{tar_file}"
+    
+    test_mbox_file_path = f"{resource_dir}/{tar_file[:-7]}/{list_name}/2013-05.mbox"
 
-    extract_tarfile(tar_file, data_dirname + "/" + list_name + "")
+    extract_tarfile(tar_path, resource_dir)
+    
+    mbox_to_eml(test_mbox_file_path, out_dir=f"{mailbox_dir}/{list_name}")
 
-    mbox_to_eml(mbox_file_path, out_dir=f"{data_dirname}/{list_name}/{mbox_file_path}")
 
-main()
+if __name__ == "__main__":
+    main()
